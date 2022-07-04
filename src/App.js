ed19ce2711f2e59e39a8e89fe926b3fae5bddbee
RAED AWILL <57587026+rodaw92@users.noreply.github.com>
2022-07-04 12:34:50 +0100

    Create a board

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { useEffect, useState } from 'react';
 
 
 const API_URL = 'https://api.frontendexpert.io/api/fe/wordle-words';
-
+const WORD_LEGNTHt = 5;
 //const API_URL = new URL('https://api.frontendexpert.io/api/fe/wordle-words')
 
 
 function App() {
 
   const [solution, setSolution] = useState('');
+  const [gusses, setGusses] = useState(Array(6).fill(null)); // to keep track for all word that guessed
   useEffect(() => {
 
     const fetchWord = async () => {
@@ -23,11 +24,29 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      {solution}
+    <div className="board">
+      { // to craete a board
+        gusses.map(guess => {
+          return (
+            <Line guess={guess ?? ''} />
+          );
+        })
+      }
 
     </div>
   );
 }
 
+function Line({ guess }) {
+  const tiles = [];
+  for (let i = 0; i < WORD_LEGNTHt; i++) {
+    const char = guess[i];
+    tiles.push(<div key={i} className='tile'>{char}</div>)
+  }
+  return (
+    <div className='line'>
+      {tiles}
+    </div>
+  )
+}
 export default App;
